cacc57dd48e8cc52d63e9dacce4555b68c1bf72a
Anil_Ramcharran <anil.gitme@gmail.com>
2021-03-19 21:07:04 -0400

    deploying app

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const apiRouter = require('./api-router.js');
+
+const server = express();
+
+server.use(helmet());
+
+server.use('/api', apiRouter);
+
+server.get('/motd', (req, res) => {
+    const motd = process.env.MOTD || 'Hello World!!!';
+    res.status(200).json({ motd: motd });
+})
+
+module.exports = server;
